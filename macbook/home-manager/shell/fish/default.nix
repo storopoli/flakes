a91942c64be3a152196e2b293e42864d8793f3f4
Jose Storopoli <jose@storopoli.io>
2023-11-23 11:13:32 -0300

    fix(fish): JULIA_NUM_THREADS to auto

--- macbook/home-manager/shell/fish/default.nix
+++ macbook/home-manager/shell/fish/default.nix
@@ -27,6 +27,9 @@
       --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
       --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
+      # Julia
+      set -Ux JULIA_NUM_THREADS "auto"
+
       # name: 'Catppuccin mocha'
       # url: 'https://github.com/catppuccin/fish'
       # preferred_background: 1e1e2e
